4750fd9b102d28bc67a3108e8f89485494ca7353
putudoni <putuwigadoni@yahoo.com>
2020-03-23 21:07:28 +0700

    Testing message sender

--- src/com/pnwd/spring/jms/controller/SenderController.java
+++ src/com/pnwd/spring/jms/controller/SenderController.java
@@ -3,10 +3,13 @@
  */
 package com.pnwd.spring.jms.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.pnwd.spring.jms.service.MessageSender;
 
 /**
  * @author Putu Ngurah Wigadoni
@@ -15,9 +18,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class SenderController {
 
+	private MessageSender messageSender;
+	
+	@Autowired
+	public void setMessageSender(MessageSender messageSender) {
+		this.messageSender = messageSender;
+	}
+
 	@RequestMapping(value = "/sender", method=RequestMethod.GET)
 	public @ResponseBody String sender() {
-		return "sender";
+		messageSender.sendMail();
+		return "Messages sent successfully";
 	}
 	
 }
